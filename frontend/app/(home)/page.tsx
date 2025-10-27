fd0867cc3fc42ae18b6554bfe7f1e218251da710
ARTHURpvn <arthursppavan@gmail.com>
2025-10-26 22:32:51 -0300

    Add initial project setup: configure PostCSS, ESLint, TypeScript, and create basic components for UI, creating the initia login system

---
+++ frontend/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import LoginPage from "../(auth)/login/page";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+        <LoginPage />
+    </div>
+  );
+}
+
+export default Home;
