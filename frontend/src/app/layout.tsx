bfd571aa45081855a7d5edd86fc559cbcdd97ef3
ARTHURpvn <arthursppavan@gmail.com>
2026-01-21 16:07:28 -0300

    Update Google site verification metadata in layout component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import AuthAwareLayout from "../components/AuthAwareLayout";
 import { useIdeaStore } from "@/store/idea_store";
@@ -119,6 +120,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NLJZSTHDX7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NLJZSTHDX7');
+          `}
+        </Script>
+
         <Analytics />
         <SpeedInsights />
 
